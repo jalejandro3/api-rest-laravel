69271924d938ca2fa8533a44534075147c94d69b
José Alvarez <jose.alvarez@codeicus.com>
2020-05-18 08:52:55 -0300

    Adding and managing exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,22 @@
 
 namespace App\Exceptions;
 
+use App\Exceptions\Service\ServiceException;
+use Carbon\Carbon;
+use GuzzleHttp\Exception\ClientException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\UnauthorizedException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response as ResponseCode;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 use Throwable;
+use Exception;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\JsonResponse;
 
 class Handler extends ExceptionHandler
 {
@@ -13,7 +27,10 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
+        AuthorizationException::class,
+        HttpException::class,
+        ModelNotFoundException::class,
+        ValidationException::class,
     ];
 
     /**
@@ -27,12 +44,50 @@ class Handler extends ExceptionHandler
     ];
 
     /**
+     * Errors list.
+     *
+     * @var array
+     */
+    private $errors = [
+        [
+            'class' => ApplicationException::class,
+            'error_message' => 'Bad Request.',
+            'response_code' => ResponseCode::HTTP_BAD_REQUEST
+        ],
+        [
+            'class' => UnauthorizedException::class,
+            'error_message' => 'Access Denied.',
+            'response_code' => ResponseCode::HTTP_UNAUTHORIZED
+        ],
+        [
+            'class' => ServiceException::class,
+            'error_message' => 'Internal System Error.',
+            'response_code' => ResponseCode::HTTP_INTERNAL_SERVER_ERROR
+        ],
+        [
+            'class' => ClientException::class,
+            'error_message' => 'Internal System Error.',
+            'response_code' => ResponseCode::HTTP_INTERNAL_SERVER_ERROR
+        ],
+        [
+            'class' => ProcessFailedException::class,
+            'error_message' => 'Internal System Error.',
+            'response_code' => ResponseCode::HTTP_INTERNAL_SERVER_ERROR
+        ],
+        [
+            'class' => ResourceNotFoundException::class,
+            'error_message' => 'Resource not found.',
+            'response_code' => ResponseCode::HTTP_NOT_FOUND
+        ]
+    ];
+
+    /**
      * Report or log an exception.
      *
-     * @param  \Throwable  $exception
+     * @param  Throwable  $exception
      * @return void
      *
-     * @throws \Exception
+     * @throws Exception
      */
     public function report(Throwable $exception)
     {
@@ -42,14 +97,98 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Throwable  $exception
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param  Request  $request
+     * @param  Throwable $exception
+     * @return Response
      *
-     * @throws \Throwable
+     * @throws Throwable
      */
     public function render($request, Throwable $exception)
     {
-        return parent::render($request, $exception);
+        if ($error = $this->exceptionExists($exception)) {
+            return $this->getResponse($exception, $error);
+        }
+
+        return $this->getResponse($exception);
+    }
+
+    /**
+     * Check if error exists in error array
+     *
+     * @param Exception $exception Exception to find into errors array.
+     * @return Exception|bool
+     */
+    private function exceptionExists(Exception $exception)
+    {
+        foreach ($this->errors as $error) {
+            if ($exception instanceof $error['class']) {
+                return $error;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Configura el mensaje de error a devolver.
+     *
+     * @param Exception $exception
+     * @param null $error
+     * @return JsonResponse
+     */
+    private function getResponse(Exception $exception, $error = null)
+    {
+        if (! is_null($error)) {
+            $errorMessage = $this->getMessageError($exception, $error);
+            $responseCode = $this->getResponseCode($exception, $error);
+            $response = $this->getErrorResponse($errorMessage, $exception, $responseCode);
+        } else {
+            $response = $this->getErrorResponse('Ha ocurrido un error!', $exception, 500);
+        }
+
+        return $response;
+    }
+
+    /**
+     * Return error response.
+     *
+     * @param string $message
+     * @param int $code
+     * @param Exception $exception
+     * @return JsonResponse
+     */
+    protected function getErrorResponse($message, $exception, $code)
+    {
+        return response()->json([
+            'timestamp' => Carbon::now(),
+            'error' => $exception->getMessage(),
+            'message' => $message
+        ], $code);
+    }
+
+    /**
+     * Return error message.
+     *
+     * @param Exception $exception
+     * @param $error
+     * @return string
+     */
+    private function getMessageError(Exception $exception, $error): string
+    {
+        return ($exception->getMessage() === ''
+            && isset($error['error_message'])) ? $error['error_message'] : $exception->getMessage();
+    }
+
+    /**
+     * Return response code.
+     *
+     * @param Exception $exception
+     * @param $error
+     * @return int|mixed
+     */
+    private function getResponseCode(Exception $exception, $error)
+    {
+        return ($exception->getCode() === 0 &&
+            isset($error['response_code'])) ? $error['response_code'] : $exception->getCode();
     }
 }
